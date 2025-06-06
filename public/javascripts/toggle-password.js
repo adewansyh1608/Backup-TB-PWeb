fc6ddca2566ae4dfeadb7658102654a1a9c6a36d
adewansyh1608 <2311523001_andre@student.unand.ac.id>
2025-06-06 21:51:16 +0700

    new

--- public/javascripts/toggle-password.js
+++ public/javascripts/toggle-password.js
@@ -1,10 +1,15 @@
-function toggleVisibility(inputId, img) {
-  const input = document.getElementById(inputId);
-  if (!input) return;
+function toggleVisibility() {
+  const passwordField = document.getElementById("password");
+  const eyeIcon = document.getElementById("eye-icon");
 
-  const isPassword = input.type === "password";
-  input.type = isPassword ? "text" : "password";
-  img.src = isPassword ? "/images/hide.png" : "/images/view.png";
+  // Toggle the type between 'password' and 'text'
+  if (passwordField.type === "password") {
+    passwordField.type = "text";
+    eyeIcon.src = "/images/hide.png"; // Change to hide icon
+  } else {
+    passwordField.type = "password";
+    eyeIcon.src = "/images/view.png"; // Change to view icon
+  }
 }
 
 function validateForm(event) {
